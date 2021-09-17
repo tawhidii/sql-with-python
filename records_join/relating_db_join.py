e0202531b412764c5de2b620a7703b0b2bfe6778
tawhidii <barii.py@gmail.com>
2021-09-17 14:44:50 +0600

    Added inner,left outer,right outer,full join example

--- records_join/relating_db_join.py
+++ records_join/relating_db_join.py
@@ -10,8 +10,55 @@ def fecth_with_join_one():
     data = pandas.read_sql_query(query,conn)
     conn.close()
     print('executed!!')
-    print(pandas.DataFrame.head(data))
+    print(data)
 
-fecth_with_join_one()
+# fecth_with_join_one()
 
 
+def fecth_with_join_inner():
+    query = """
+    SELECT url,username FROM photos
+        INNER JOIN users ON users.id = photos.user_id;
+    """
+    data = pandas.read_sql_query(query,conn)
+    conn.close()
+    print('executed!!')
+    print(data)
+
+
+# fecth_with_join_inner()
+
+def fetch_with_join_left_outer():
+    query = """
+    SELECT url,username FROM photos
+        LEFT JOIN users ON users.id = photos.user_id;
+    """
+    data = pandas.read_sql_query(query,conn)
+    conn.close()
+    print('executed !')
+    print(data)
+
+# fetch_with_join_left_outer()
+
+
+def fetch_with_join_right_outer():
+    query = """
+    SELECT url,username FROM photos
+        RIGHT JOIN users ON users.id = photos.user_id;
+    """
+    data = pandas.read_sql_query(query,conn)
+    print(data)
+
+ 
+# fetch_with_join_right_outer()
+
+
+def fetch_with_full_join():
+    query = """
+    SELECT url,username FROM photos
+        FULL  JOIN users ON users.id = photos.user_id;
+    """
+    data = pandas.read_sql_query(query,conn)
+    print(data)
+
+fetch_with_full_join()
